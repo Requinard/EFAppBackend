309821f6859bdca8da8cc94d8829544a82170866
David <david.diks@student.fontys.nl>
2016-01-20 14:04:36 +0100

    Update views.py

--- src/api/views.py
+++ src/api/views.py
@@ -13,18 +13,3 @@ class DeviceView(viewsets.ModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
 
-    permission_classes = [permissions.AllowAny]
-
-    def get_list(self, request, pk=None):
-        queryset = Device.objects.none()
-
-        if request.user.is_superuser:
-            queryset = Device.objects.all()
-
-        if pk is not None:
-            queryset = Device.objects.get(device_id=pk)
-
-        serializer = self.get_serializer(queryset)
-
-        return Response(serializer.data)
-
